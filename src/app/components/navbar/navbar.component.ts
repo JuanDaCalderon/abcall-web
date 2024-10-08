e822d56e44e17da3d0422b2687ca953658970328
sneyder53 <sneyderamado.com@gmail.com>
2024-10-08 17:35:33 -0500

    subiendo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {RouterOutlet, RouterLink, RouterLinkActive} from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
