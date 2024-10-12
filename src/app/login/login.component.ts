704a28d5942e72c424f003aa6ec39508d5b1f1ab
lacortesz <la.cortesz1@uniandes.edu.co>
2024-10-11 21:16:13 -0500

    Inclusion icono internacionalizacion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,18 @@
 import {CommonModule} from '@angular/common';
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+import {TranslateModule} from '@ngx-translate/core';
 //import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, TranslateModule],
   standalone: true
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-
-  constructor(
-    private formBuilder: FormBuilder
-    //private toastr: ToastrService
-  ) {}
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
@@ -25,9 +21,20 @@ export class LoginComponent implements OnInit {
     });
   }
 
+  /*constructor(private formBuilder: FormBuilder, private translate: TranslateService) {
+      translate.addLangs(['en', 'es']);
+      translate.setDefaultLang('es');
+    }*/
+
+  constructor(private formBuilder: FormBuilder) {}
+
   validateCredentials() {
     console.info('Credenciales correctas');
     //this.toastr.success("Confirmation", "Author created")
     this.loginForm.reset();
   }
+
+  //changeLanguage(lang: string) {
+  //  this.translate.use(lang);
+  //}
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,10 +3,30 @@ import {CommonModule} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
 import {LoginComponent} from './login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {provideHttpClient, HttpClient} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
+// Función que carga los archivos de traducción
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
-  imports: [CommonModule, BrowserModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
   declarations: [LoginComponent],
-  exports: [LoginComponent, BrowserModule, FormsModule, ReactiveFormsModule]
+  exports: [LoginComponent, BrowserModule, FormsModule, ReactiveFormsModule, TranslateModule],
+  providers: [provideHttpClient()]
 })
 export class LoginModule {}
