b15f3c02affe569fc9a958ba30c73722ccdc9cd9
IngSneyderAmado <s.amadov@uniandes.edu.co>
2024-10-16 20:17:31 -0500

    navar , enrutamiento , environment

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -2,11 +2,12 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {Cliente} from '../models/cliente';
+import {environment} from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-  private url = 'http://localhost:8003/';
+  private url = environment.urlApi + environment.portUsuario + '/';
   constructor(private _http: HttpClient) {}
   createCliente(client: Cliente): Observable<Cliente> {
     return this._http.post<Cliente>(this.url + 'usuario/cliente', client);
