909e06a748a3b3cfbb63a1569a32c0ebdd4d38f3
lacortesz <la.cortesz1@uniandes.edu.co>
2024-10-18 18:57:03 -0500

    Finalizacion pruebas cypress

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -26,7 +26,7 @@ describe('LoginComponent E2E Tests', () => {
         cy.get('button[type="submit"]').click();
 
        //cy.wait('@loginRequest');
-        cy.get('div[id="authFlag"]').should('contain', 'Datos de usuario incorrectos');
+        cy.get('div[id="authFlagError"]').should('contain', 'Datos de usuario incorrectos');
     });
 
     /*fit('should display success message on login success', () => {
@@ -55,14 +55,51 @@ describe('LoginComponent E2E Tests', () => {
 
     it('should validate password field as required and minimum length', () => {
         cy.get('input[id="password"]').focus().blur();
-        cy.get('.passwordRequired').should('contain', 'Password is required');
+        cy.get('div[id="passwordRequired"]').should('contain', 'La contraseña es obligatoria');
 
-        cy.get('input[id="passwordMinLength"]').type('short').blur();
-        cy.get('.password-error').should('contain', 'Password must be at least 6 characters');
+        cy.get('input[id="password"]').type('short').blur();
+        cy.get('div[id="passwordMinLength"]').should('contain', 'La contraseña debe tener al menos 8 caracteres');
     });
 
     it('should change language when changeLang is called', () => {
-        cy.get('.language-selector').select('en');
-        cy.get('html').should('have.attr', 'lang', 'en');
+        cy.get('img[id="languageButton"]').click();
+        cy.get('label[id="emailLabel"]').should('contain', 'Email');
+        cy.get('label[id="passwordLabel"]').should('contain', 'Password');
+        cy.get('button[type="submit"]').should('contain', 'Login');
+    });
+
+    //--------
+
+    it('should display error message on login failure in english', () => {
+        cy.get('img[id="languageButton"]').click();
+        cy.intercept('POST', '/api/auth/login', {
+            statusCode: 401,
+            body: { error: { message: 'Datos de usuario incorrectos' } },
+        }).as('loginRequest');
+
+        cy.get('input[id="email"]').type('test@test.com');
+        cy.get('input[id="password"]').type('password123');
+        cy.get('button[type="submit"]').click();
+
+       //cy.wait('@loginRequest');
+        cy.get('div[id="authFlagError"]').should('contain', 'Incorrect user data');
+    });
+
+    it('should validate email field as required and email format in english', () => {
+        cy.get('img[id="languageButton"]').click();
+        cy.get('input[id="email"]').focus().blur();
+        cy.get('div[id="emailRequired"]').should('contain', 'Email is required');
+
+        cy.get('input[id="email"]').type('invalid-email').blur();
+        cy.get('div[id="emailFormatError"]').should('contain', 'The email must be in email format');
+    });
+
+    it('should validate password field as required and minimum length in english', () => {
+        cy.get('img[id="languageButton"]').click();
+        cy.get('input[id="password"]').focus().blur();
+        cy.get('div[id="passwordRequired"]').should('contain', 'Password is required');
+
+        cy.get('input[id="password"]').type('short').blur();
+        cy.get('div[id="passwordMinLength"]').should('contain', 'Password must be at least 8 characters long');
     });
 });
