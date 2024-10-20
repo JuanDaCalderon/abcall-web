87cdab50ecacd57bed288be42b903d8ecd9f208f
Leonardo Barrios Poveda <o.barriosp@uniandes.edu.co>
2024-10-19 22:16:34 -0500

    Avance unit test

--- src/app/incidencias/list-incidencias/list-incidencias.component.spec.ts
+++ src/app/incidencias/list-incidencias/list-incidencias.component.spec.ts
@@ -1,22 +1,103 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {TranslateModule, TranslateLoader, TranslateFakeLoader} from '@ngx-translate/core';
 import {ListIncidenciasComponent} from './list-incidencias.component';
+import {IncidenciasService} from '../../services/incidencias.service';
+import {of} from 'rxjs';
+import {Incidente} from '../../models/incidentes';
 
 describe('ListIncidenciasComponent', () => {
   let component: ListIncidenciasComponent;
   let fixture: ComponentFixture<ListIncidenciasComponent>;
+  let incidenciasService: IncidenciasService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListIncidenciasComponent]
+      imports: [
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        }),
+        ListIncidenciasComponent
+      ],
+      providers: [IncidenciasService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ListIncidenciasComponent);
     component = fixture.componentInstance;
+    incidenciasService = TestBed.inject(IncidenciasService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load incidencias on init', () => {
+    const mockIncidencias: Incidente[] = [
+      {
+        ID: '1',
+        DESCRIPCION: 'Test Incidente',
+        CLIENTE: 'Cliente Test',
+        COMENTARIOS: 'Comentarios Test',
+        CORREO: 'CorreoTest@correotest.com',
+        DIRECCION: 'Dirección Test',
+        ESTADO: 'Abierto',
+        FECHACREACION: '19-10-2024 11:39:00',
+        PRIORIDAD: 'Alta',
+        TELEFONO: '111111111',
+        USUARIO: 'Usuario Test'
+      },
+      {
+        ID: '1',
+        DESCRIPCION: 'Test Incidente',
+        CLIENTE: 'Cliente Test',
+        COMENTARIOS: 'Comentarios Test',
+        CORREO: 'CorreoTest@correotest.com',
+        DIRECCION: 'Dirección Test',
+        ESTADO: 'Abierto',
+        FECHACREACION: '19-10-2024 11:39:00',
+        PRIORIDAD: 'Alta',
+        TELEFONO: '111111111',
+        USUARIO: 'Usuario Test'
+      }
+    ];
+    spyOn(incidenciasService, 'getIncidencias').and.returnValue(of(mockIncidencias));
+
+    component.getIncidencias();
+  });
+
+  it('should reload incidencias', () => {
+    const mockIncidencias: Incidente[] = [
+      {
+        ID: '1',
+        DESCRIPCION: 'Incidencia 1',
+        ESTADO: 'Abierto',
+        PRIORIDAD: 'Alta',
+        CLIENTE: 'Cliente 1',
+        USUARIO: 'Usuario 1',
+        COMENTARIOS: '',
+        CORREO: '',
+        DIRECCION: '',
+        FECHACREACION: '',
+        TELEFONO: ''
+      }
+    ];
+
+    spyOn(incidenciasService, 'getIncidencias').and.returnValue(of(mockIncidencias));
+
+    component.reloadIncidencias();
+  });
+
+  it('should change language', () => {
+    spyOn(component.translate, 'use');
+
+    component.changeLang('en');
+
+    expect(component.language).toBe('en');
+    expect(component.translate.use).toHaveBeenCalledWith('en');
   });
 });
--- src/app/incidencias/list-incidencias/list-incidencias.component.ts
+++ src/app/incidencias/list-incidencias/list-incidencias.component.ts
@@ -1,18 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, inject} from '@angular/core';
 import {IncidenciasService} from '../../services/incidencias.service';
 import {Incidente} from '../../models/incidentes';
 import {NgFor, NgIf} from '@angular/common';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import {Subject, takeUntil} from 'rxjs';
 
 @Component({
   selector: 'app-list-incidencias',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, TranslateModule],
   providers: [IncidenciasService],
   templateUrl: './list-incidencias.component.html',
   styleUrl: './list-incidencias.component.scss'
 })
 export class ListIncidenciasComponent implements OnInit {
   incidencias: Incidente[] = [];
+  language = 'es';
+  translate: TranslateService = inject(TranslateService);
+  private destroy$ = new Subject<void>();
 
   constructor(private incidenciasService: IncidenciasService) {}
 
@@ -21,17 +26,23 @@ export class ListIncidenciasComponent implements OnInit {
   }
 
   getIncidencias() {
-    this.incidenciasService.getIncidencias().subscribe(
-      (data: Incidente[]) => {
-        this.incidencias = data;
-        console.log(this.incidencias[0]);
-      },
-      (error) => {
-        console.error('Error al obtener la lista de incidencias', error);
-      }
-    );
+    this.incidenciasService
+      .getIncidencias()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (data: Incidente[]) => {
+          this.incidencias = data;
+        },
+        (error) => {
+          console.error('Error al obtener la lista de incidencias', error);
+        }
+      );
   }
   reloadIncidencias(): void {
     this.getIncidencias();
+  }
+  changeLang(lang: string): void {
+    this.language = lang;
+    this.translate.use(lang);
   }
 }
