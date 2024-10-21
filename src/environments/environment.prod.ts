15a78f69480dee023ece4817e92c700b78c12e33
IngSneyderAmado <s.amadov@uniandes.edu.co>
2024-10-21 01:23:07 -0500

    http

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlApi: 'https://bl-app-abcall-1267046842.us-east-1.elb.amazonaws.com:',
+  urlApi: 'http://bl-app-abcall-1267046842.us-east-1.elb.amazonaws.com:',
   portRole: '8002',
   portUsuario: '8003',
   portConsulIncidencias: '8001',
