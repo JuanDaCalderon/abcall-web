8612bc2fd6be20378259caeab1c6fa11c16768d1
Leonardo Barrios Poveda <o.barriosp@uniandes.edu.co>
2024-11-17 17:24:08 -0500

    Feature/ABC-31

---
+++ src/app/configuracion/personalizar-textos/personalizar-textos.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-personalizar-textos',
+  templateUrl: './personalizar-textos.component.html',
+  styleUrl: './personalizar-textos.component.css'
+})
+export class PersonalizarTextosComponent {}
